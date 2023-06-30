384cdc87a0e6996280f9ad5ffb2174c8a6db47e0
AHMET TURAN <akio@msn.com>
2023-07-01 02:17:57 +0300

    not

--- resources/views/front/pages/index.blade.php
+++ resources/views/front/pages/index.blade.php
@@ -7,25 +7,6 @@
 @endsection
 
 @section("content")
-    {{--    <div class="site-blocks-cover" style="background-image: url({{ asset($slider->image ?? "images/hero_1.jpg") }});"--}}
-    {{--         data-aos="fade">--}}
-
-    {{--        <div class="container">--}}
-
-    {{--            <div class="row align-items-start align-items-md-center justify-content-end">--}}
-    {{--                <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">--}}
-    {{--                        <h1 class="mb-2">{{ $slider->name ?? "empty" }}</h1>--}}
-    {{--                        <div class="intro-text text-center text-md-left">--}}
-    {{--                            <p class="mb-4">{{ $slider->content ?? "empty" }} </p>--}}
-    {{--                            <p>--}}
-    {{--                                <a href="{{ url($slider["shop_url"]) ?? "empty" }}" class="btn btn-sm btn-primary">Alışverişe--}}
-    {{--                                    Başla</a>--}}
-    {{--                            </p>--}}
-    {{--                        </div>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-    {{--    </div>--}}
 
     <!-- start of hero -->
     <section class="hero-slider hero-style">

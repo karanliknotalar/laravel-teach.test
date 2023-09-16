66d4392e2f21d71e2368fafc33a10116e9de9a8a
Ahmet Turan <akio@msn.com>
2023-09-16 10:15:22 +0300

    small edit for products page view

--- resources/views/front/pages/products.blade.php
+++ resources/views/front/pages/products.blade.php
@@ -13,7 +13,7 @@
             <div class="row mb-5">
                 <div class="col-md-9 order-2">
                     <div class="row">
-                        <div class="col-md-12 mb-5">
+                        <div class="col-md-12 mb-2">
                             <div class="float-md-left mb-4"><h2 class="text-black h5">Ürünler</h2></div>
                             <div class="d-flex">
                                 <div class="dropdown mr-1 ml-md-auto btn-group">

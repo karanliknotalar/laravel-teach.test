73f0a3e5a00bf041169b41d61244fbb7ac214b20
Ahmet Turan <akio@msn.com>
2023-09-20 13:28:53 +0300

    small fix for amin page dashboard

--- resources/views/components/admin/index/change-rate-area.blade.php
+++ resources/views/components/admin/index/change-rate-area.blade.php
@@ -2,10 +2,10 @@
     <p class="mb-0 text-muted">
         @if($rate > 0)
             <span class="text-success me-2"><i
-                    class="mdi mdi-arrow-up-bold"></i> {{ (gettype($rate) === "double" or $rate < 100) ? number_format($rate, 2) : $rate }}%</span>
+                    class="mdi mdi-arrow-up-bold"></i> {{ (gettype($rate) === "double" and $rate < 100) ? number_format($rate, 2) : (int)$rate }}%</span>
         @else
             <span class="text-danger me-2"><i
-                    class="mdi mdi-arrow-down-bold"></i> {{ (gettype($rate) === "double" or $rate < 100) ? number_format($rate, 2) : $rate }}%</span>
+                    class="mdi mdi-arrow-down-bold"></i> {{ (gettype($rate) === "double" and $rate < 100) ? number_format($rate, 2) : (int)$rate }}%</span>
         @endif
         <span class="text-nowrap">Geçen aydan beri</span>
     </p>
